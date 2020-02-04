d298ebaa03cb17798ec2f194a116026fa96031a2
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-02-04 13:18:50 +1300

    Adds Github Logo

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-
+import GithubLogo from "../Resources/GitHub.png"
 class HeaderComponent extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {};
-  }
+        this.state = {};
+    }
 
-  render() {
-    return (
-        <div className="container-fluid bg-dark" style={{textAlign : "center", height: "25%"}}>
-        <h1 className="display-5 text-white">Github Login</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+
+            <div className="container-fluid bg-dark" style={{ textAlign: "center", height: "150%" }}>
+                <img src={GithubLogo} alt="Github logo" style={{ height: "5%", width: "5%" }} />
+                <h1 className="display-5 text-white">Github Login</h1>
+            </div>
+        )
+    }
 }
 
 export default HeaderComponent;
