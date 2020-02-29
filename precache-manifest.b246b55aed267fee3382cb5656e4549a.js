6789900e91eff021d7af7d86eb6d519a561eb4d3
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-02-29 13:00:21 +1300

    Updates

---
+++ precache-manifest.b246b55aed267fee3382cb5656e4549a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d625e42a451f0a22fd2348f0a9965a",
+    "url": "/Github-Search/index.html"
+  },
+  {
+    "revision": "3e1cb16047eba11a31d3",
+    "url": "/Github-Search/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1d42eda7c71c21ae78a7",
+    "url": "/Github-Search/static/css/main.74db5c72.chunk.css"
+  },
+  {
+    "revision": "3e1cb16047eba11a31d3",
+    "url": "/Github-Search/static/js/2.2047f9ac.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Github-Search/static/js/2.2047f9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d42eda7c71c21ae78a7",
+    "url": "/Github-Search/static/js/main.4473ee76.chunk.js"
+  },
+  {
+    "revision": "40cb01dd7bb57dc3bb16",
+    "url": "/Github-Search/static/js/runtime-main.35f24de3.js"
+  }
+]);
